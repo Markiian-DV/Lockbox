dae3f443857c3fdc109ec9cdfdc192403890fe00
Markiian <gvintua@gmail.com>
2025-03-17 19:04:21 +0200

    fix test endpoint

--- src/Lockbox.Web/Controllers/TestController.cs
+++ src/Lockbox.Web/Controllers/TestController.cs
@@ -4,13 +4,13 @@ using Microsoft.AspNetCore.Mvc;
 namespace Lockbox.Web.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class TestController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
+    private static readonly string[] Summaries =
+    [
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+    ];
 
     private readonly ILogger<TestController> _logger;
 
@@ -22,13 +22,12 @@ public class TestController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return [.. Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        })];
     }
 
     [Authorize]
